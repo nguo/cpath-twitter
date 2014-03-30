fc81015203a6e5c996da1be28fbf3f7f80c8f90d
nguo <nguo@zynga.com>
2014-03-29 17:17:13 -0700

    add endless scrolling and cleaned up the way the timeline activity sets up the adapter

--- src/codepath/apps/twitter/activities/TimelineActivity.java
+++ src/codepath/apps/twitter/activities/TimelineActivity.java
@@ -2,11 +2,11 @@ package codepath.apps.twitter.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 import codepath.apps.twitter.R;
 import codepath.apps.twitter.TwitterApp;
 import codepath.apps.twitter.adapters.TweetsAdapter;
+import codepath.apps.twitter.helpers.EndlessScrollListener;
 import codepath.apps.twitter.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
@@ -17,26 +17,50 @@ import java.util.ArrayList;
  * TimelineActivity - home timeline screen
  */
 public class TimelineActivity extends Activity {
+	// views
 	private ListView lvTweets;
+
+	/** list of tweets */
+	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+	/** tweets adapter */
 	private TweetsAdapter adapter;
 
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
 		setupViews();
-
-		TwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONArray jsonTweets) {
-				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
-				adapter = new TweetsAdapter(getBaseContext(), tweets);
-				lvTweets.setAdapter(adapter);
-			}
-		});
+		setupListeners();
+		getMoreTweets(true);
 	}
 
 	/** setups the views */
 	private void setupViews() {
 		lvTweets = (ListView) findViewById(R.id.lvTweets);
+		adapter = new TweetsAdapter(getBaseContext(), tweets);
+		lvTweets.setAdapter(adapter);
+	}
+
+	/** setups the listeners on the views */
+	private void setupListeners() {
+		lvTweets.setOnScrollListener(new EndlessScrollListener() {
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				getMoreTweets(false);
+			}
+		});
+	}
+
+	/** makes request to get more tweets */
+	private void getMoreTweets(final boolean clearList) {
+		TwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				if (clearList) {
+					adapter.clear();
+				}
+				adapter.addAll(Tweet.fromJson(jsonTweets));
+			}
+		});
 	}
 }
